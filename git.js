b4cab89509fceaf49e34558fe5570b205a39d450
qvstpe6gocggyt2hoayu3eeiecrcowi <114455281+qvstpe6gocggyt2hoayu3eeiecrcowi@users.noreply.github.com>
2022-09-27 23:49:05 +0700

    Add files via upload

---
+++ git.js
@@ -0,0 +1,48 @@
+/*! Linux Software */
+function blender_link(){window.open('https://blender.org');}
+function krita_link(){window.open('https://krita.org');}
+function gimp_link(){window.open('https://gimp.org');}
+function inkscape_link(){window.open('https://inkscape.org');}
+function kdenlive_link(){window.open('https://kdenlive.org');}
+
+
+/*! Platforms */
+function github_link(){window.open('https://pages.github.com');}
+function blogger_link(){window.open('https://blogger.com');}
+function wordpress_link(){window.open('https://wordpress.org');}
+
+
+/*! Programming Language */
+function html_link(){window.open('https://en.wikipedia.org/wiki/HTML5');}
+function css_link(){window.open('https://en.wikipedia.org/wiki/CSS');}
+function javascript_link(){window.open('https://javascript.com');}
+function jekyll_link(){window.open('https://jekyllrb.com');}
+function ruby_link(){window.open('https://www.ruby-lang.org');}
+
+
+/*! Linux System Operating */
+function linux_link(){window.open('https://linux.org');}
+function tails_link(){window.open('https://tails.boum.org');}
+function debian_link(){window.open('https://debian.org');}
+function kali_link(){window.open('https://kali.org');}
+function ubuntu_link(){window.open('https://ubuntu.com');}
+function studio_link(){window.open('https://ubuntustudio.org');}
+function mint_link(){window.open('https://linuxmint.com');}
+function backtrack_link(){window.open('https://backtrack-linux.org');}
+function slackware_link(){window.open('http://slackware.com');}
+
+/*! Windows Software */
+function android_link(){window.open('https://developer.android.com/studio');}
+function audacity_link(){window.open('https://www.audacityteam.org');}
+function mf8_link(){window.open('https://en.wikipedia.org/wiki/Adobe_Flash');}
+function ap7_link(){window.open('https://adobe.fandom.com/wiki/Adobe_Photoshop_7');}
+function cd12_link(){window.open('https://web.archive.org/web/20050306121213/http://www.corel.com/servlet/Satellite?pagename=Corel%2FCompany%2FPressRoom%2FPressRelease&id=1044393442588&prid=1047022861547');}
+function uvs8_link(){window.open('https://en.wikipedia.org/wiki/Corel_VideoStudio');}
+function psu12_link(){window.open('https://en.wikipedia.org/wiki/Pinnacle_Studio');}
+function wmm2_link(){window.open('https://en.wikipedia.org/wiki/Windows_Movie_Maker');}
+
+/*! Windows System Operating */
+function xp_link(){window.open('https://support.microsoft.com/en-us/windows/windows-xp-support-has-ended-47b944b8-f4d3-82f2-9acc-21c79ee6ef5e');}
+function vista_link(){window.open('https://www.microsoft.com/en-us/download/details.aspx?id=13158');}
+function seven_link(){window.open('https://www.microsoft.com/en-US/windows/windows-7-end-of-life-support-information');}
+function ten_link(){window.open('https://www.microsoft.com/en-us/software-download/windows10ISO');}
